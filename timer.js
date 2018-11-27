235aefde3896ceca9fe7469c84abe9b24077a71d
500x500 <40839392+500x500@users.noreply.github.com>
2018-11-27 23:00:56 +0300

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,30 @@
+
+var clock = document.getElementById("clock");
+var color = document.getElementById("color");
+
+function hexoClock(){
+    var time = new Date();
+    var h = (time.getHours()%12).toString();
+    var m = time.getMinutes().toString();
+    var s = time.getSeconds().toString();
+
+    if(h.length<2){
+        h = '0' + h;
+    }
+    if(m.length<2){
+        h = '0' + h;
+    }
+    if(s.length<2){
+        h = '0' + h;
+    }
+
+    var clockString = h + ":" + m + ":" + s;
+    var colorString = "#" + h + m + s;
+
+    clock.textContent = clockString;
+    color.textContent = colorString;
+    document.body.style.background = colorString;
+}
+
+setInterval(hexoClock, 1000);
+
